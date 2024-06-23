234712a25959e53c51ff0cb9daaf878220573626
Vivek7038 <vivek888chavan@gmail.com>
2024-06-23 15:14:04 +0530

    updated dummy job posts

--- src/data/JobPostsData.js
+++ src/data/JobPostsData.js
@@ -2,22 +2,30 @@ export const JobPostData = [
   {
     id: 1,
     title: 'Software Engineer 1',
-    introduction: 'We are looking for a Software Engineer...',
-    rolesAndResponsibilities: 'Develop and maintain software applications...',
+    introduction: `Were always looking for talented, passionate people like you.
+Diversity & Inclusion are fundamental to our culture, and Aicence is proud to be an equal opportunity workplace and an affirmative-action employer. All employment decisions at Aicence are based on business`,
+    rolesAndResponsibilities: `Develop and maintain user-facing features using React.js.
+Experience with popular React.js workflows(such as Redux or Context API).
+Knowledge of modern front- end build pipelines and tools(e.g., Webpack, Babel, NPM).
+Familiarity with RESTful APIs And TypeScript.
+Proven experience as a React.js Developer, with a minimum of[4] years in a senior or lead role.
+Familiarity with newer specifications of ECMAScript.
+Knowledge of modern authorization mechanisms, such as JSON Web Token(JWT).'`,
     experienceRange: {
       min: 2,
       max: 5,
     },
-    qualifications: 'Bachelor\'s degree in Computer Science...',
+    qualifications: `At least 3 years of Frontend development experience.
+A degree in information technology/management, or related fields.`,
     salaryRange: {
       min: 6,
       max: 12,
     },
-    callToAction: 'Apply now to be a part of an exciting team...',
+    callToAction: `Thank you for your interest in this open position with us. Would you like to apply for this role, please feel free to share your updated profile at xyz@xyz.com`,
     company: 'Tech Solutions Ltd.',
     jobLocation: 'New York, NY',
     jobType: 'Full Time',
-    labels: [],
+    labels: ['Remote', '5 Day week'],
     labelOptions: ["Remote", "5 Day week"],
   },
   {
